7651aee4929648fadac306759eff9e80e5355009
Ben Coleman <becolem@microsoft.com>
2018-01-24 11:00:49 +0000

    Routes

--- site/routes/routes.js
+++ site/routes/routes.js
@@ -10,4 +10,27 @@ router
 })
 
 
+.get('/hives', function (req, res, next) {
+  res.render('hives', 
+  { 
+    title: 'Hivematic: Portal'
+  });
+})
+
+
+.get('/reports', function (req, res, next) {
+  res.render('home', 
+  { 
+    title: 'Hivematic: Reports'
+  });
+})
+
+
+.get('/admin', function (req, res, next) {
+  res.render('home', 
+  { 
+    title: 'Hivematic: Admin'
+  });
+})
+
 module.exports = router;
